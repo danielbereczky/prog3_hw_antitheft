a827147d5641439522c84da33feb630393146177
Dániel Bereczky <bereczkydaniel@edu.bme.hu>
2023-11-27 09:48:11 +0100

    moved system logic to logic.java

--- src/prog3_hw_antitheft/Logic.java
+++ src/prog3_hw_antitheft/Logic.java
@@ -1,8 +1,300 @@
 package prog3_hw_antitheft;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+
+//this class serves as the driver for the whole system
 public class Logic {
-	public static void main(String args[]){
-		Window appWindow = new Window();
+	//boolean for storing the state of the alarm system
+	public boolean isActive = true;
+	//components of the system
+	Switch hoodSwitch = new Switch();
+	Switch leftDoorSwitch = new Switch();
+	Switch rightDoorSwitch = new Switch();
+	IgnitionKey key = new IgnitionKey();
+	AlarmSiren siren = new AlarmSiren();
+	Engine carEngine = new Engine();
+	
+	CountdownTimer timer;
+	EngineTimer engTimer;
+	
+	static String code;
+	String lastEnteredCode;
+	//code setters and getters
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String newc) {
+		code = newc;
+	}
+	
+	//this method checks whether the alarm system can be armed
+	public boolean armable(){
+		if(!isActive && !carEngine.getStatus() && !hoodSwitch.getSwitchState() && !leftDoorSwitch.getSwitchState() && !rightDoorSwitch.getSwitchState() && key.getState()==IgnitionKey.keyState.OFF){
+			return true;
+		}
+		return false;
+	}
+	//system components status getters
+	public String getHoodStatus(){
+		return hoodSwitch.getStatusString();
+	}
+	public String getRDoorStatus() {
+		return rightDoorSwitch.getStatusString();
+	}
+	public String getLDoorStatus() {
+		return leftDoorSwitch.getStatusString();
+	}
+	public String getSirenStatus() {
+		return siren.getStatusString();
+	}
+	public String getEngineStatus(){
+		return carEngine.getStatusString();
+	}
+	//functions for toggling inputs
+	public void toggleLeftDoor(JLabel DoorIndicator){
+		
+		if(leftDoorSwitch.getSwitchState()) {
+			leftDoorSwitch.setSwitchState(false);
+			DoorIndicator.setText(leftDoorSwitch.getStatusString());
+		}
+		else
+			leftDoorSwitch.setSwitchState(true);
+			DoorIndicator.setText(leftDoorSwitch.getStatusString());
+	}
+	public void toggleRightDoor(JLabel DoorIndicator){
+		
+		if(rightDoorSwitch.getSwitchState()) {
+			rightDoorSwitch.setSwitchState(false);
+			DoorIndicator.setText(rightDoorSwitch.getStatusString());
+		}
+		else
+			rightDoorSwitch.setSwitchState(true);
+			DoorIndicator.setText(rightDoorSwitch.getStatusString());
+	}
+	public void toggleHood(JLabel HoodIndicator) {
+		//if the switch is open, we close it, if its not, we open it (Note: switches are closed by default)
+		if(hoodSwitch.getSwitchState()) {
+			hoodSwitch.setSwitchState(false);
+			HoodIndicator.setText(hoodSwitch.getStatusString());
+		}
+		else {
+			hoodSwitch.setSwitchState(true);
+			HoodIndicator.setText(hoodSwitch.getStatusString());
+		}
+	}
+	//system control logic functions
+	public void arm(JLabel masterStat, JLabel hint) {
+		if(armable()){
+			isActive = true;
+			masterStat.setText("ARMED");
+			hint.setText("System successfully armed");
+		}
+		else{
+			hint.setText("The system cannot be armed now");
+		}
+	}
+	public void keyTamperCheck(JLabel alarmStatus, JLabel hintText) {
+		if(isActive){
+			//if the key is touched with an armed system, we give an instant alarm
+			timer = new CountdownTimer(0,siren,alarmStatus);
+			siren.setSiren(true);
+			alarmStatus.setText(siren.getStatusString());
+			hintText.setText("You turned the key while the system was armed. Enter the code to disarm the system!");
+		}
+	}
+	//handling key inputs
+	public void keyOff(JLabel keyStatus, JLabel engineFeedBackLabel, JLabel hintText) {
+		keyStatus.setText("LOCK");
+		key.setState(IgnitionKey.keyState.OFF);
+		if(carEngine.getStatus()){
+			carEngine.stop();
+			engineFeedBackLabel.setText(carEngine.getStatusString());
+			hintText.setText("The engine is now off");
+		}
+	}
+	public void keyAcc(JLabel keyStatus) {
+		keyStatus.setText("ACC");
+		key.setState(IgnitionKey.keyState.ACC);
+	}
+	public void keyStart(JLabel keyStatus,JLabel engineFeedBackLabel, JLabel hintText) {
+		keyStatus.setText("START");
+		key.setState(IgnitionKey.keyState.START);
+		//if the alarm is not armed and the engine is not running, we can start it up
+		if(!isActive && !carEngine.getStatus() && carEngine.getStartable()){
+			carEngine.start();
+			engineFeedBackLabel.setText(carEngine.getStatusString());
+			hintText.setText("The engine is now started");
+			key.setState(IgnitionKey.keyState.ACC);
+			keyStatus.setText("ACC");
+			
+		}
+		else if(carEngine.getStatus()){
+			hintText.setText("Don't crank while the engine is running!");
+			key.setState(IgnitionKey.keyState.ACC);
+			keyStatus.setText("ACC");	
+		}
+		else{
+			key.setState(IgnitionKey.keyState.ACC);
+			keyStatus.setText("ACC");
+		}
+	}
+	public void enterCode(JPasswordField codeField, JLabel alarmStatus,JLabel masterStatus,JLabel hintText) {
+		//if the doors are closed, we can't enter a code because the panel is physically inaccessible
+		lastEnteredCode = codeField.getText();
+		codeField.setText("");
+		System.out.println(lastEnteredCode);
+		//if the siren is already running or the timer is counting down
+		if((siren.getStatus() || (timer != null && timer.isRunning())) &&  lastEnteredCode.equals(code)){
+			//disarming the system
+			timer.stop();
+			siren.setSiren(false);
+			alarmStatus.setText(siren.getStatusString());
+			isActive = false;
+			masterStatus.setText("DISARMED");
+			hintText.setText("System disarmed. You can go ahead and start the engine in 15 seconds");
+			carEngine.setStartable(true);
+			engTimer = new EngineTimer(15,carEngine,hintText);
+			engTimer.start();
+		}	
+	}
+	//this function is ran whenever there is an input which can trigger the alarm
+	public void checkAlarm(JLabel alarmStatus, JLabel hintText) {
+		//when the system is active, we need to check for triggering conditions
+		if(isActive){
+			if(hoodSwitch.getSwitchState()) {
+				//workaround, we have an instant alarm in case the hood is opened
+				timer = new CountdownTimer(0,siren,alarmStatus);
+				siren.setSiren(true);
+				alarmStatus.setText(siren.getStatusString());
+				hintText.setText("You opened the hood while the system was armed. Enter the code to disarm the system!");
+			}
+			//if we opened any of the doors while the system is active
+			else if(leftDoorSwitch.getSwitchState() || rightDoorSwitch.getSwitchState()) {
+				hintText.setText("You opened a door. Enter the code in 30 seconds to disarm the system!");
+				//countdown
+				timer = new CountdownTimer(30,siren,alarmStatus);
+				timer.start();
+			}
+		}
+	}
+	
+	//functions for serialization
+	public void write(JLabel hintText){
+		try{
+			//initializing the output data stream
+			FileOutputStream fileOut = new FileOutputStream("alarm.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			//writing the objects to the file
+			out.writeObject(isActive);
+			out.writeObject(lastEnteredCode);
+			out.writeObject(hoodSwitch);
+			out.writeObject(leftDoorSwitch);
+			out.writeObject(rightDoorSwitch);
+			out.writeObject(key);
+			out.writeObject(siren);
+			out.writeObject(carEngine);
+			out.close();
+			hintText.setText("System state saved");
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	public void read(JLabel masterStatus, JLabel hoodStatus, JLabel leftDoorStatus,JLabel rightDoorStatus, JLabel keyStatus, JLabel alarmStatus, JLabel engineFeedBackLabel, JLabel hintText, JRadioButton hoodBtn, JRadioButton leftDoorBtn, JRadioButton rightDoorBtn, JRadioButton keyOff, JRadioButton keyAcc, JRadioButton keyStart)throws ClassNotFoundException{
+		//checking whether the file to read from exists
+		Path inpPath = Paths.get("alarm.ser");
+		if(Files.exists(inpPath)){
+			//if the file exists
+			try{
+				//initializing the data stream
+				FileInputStream fileIn = new FileInputStream("alarm.ser");
+				ObjectInputStream in = new ObjectInputStream(fileIn);
+				//reading objects and setting feedback
+				isActive = (Boolean) in.readObject();
+				if(isActive){
+					masterStatus.setText("ARMED");
+				}
+				else{
+					 masterStatus.setText("DISARMED");
+				}
+				lastEnteredCode = (String) in.readObject();
+				hoodSwitch = (Switch) in.readObject();
+				hoodStatus.setText(hoodSwitch.getStatusString());
+				if(hoodSwitch.getSwitchState()){
+					hoodBtn.setSelected(true);
+				}
+				leftDoorSwitch = (Switch) in.readObject();
+				if(leftDoorSwitch.getSwitchState()){
+					leftDoorBtn.setSelected(true);
+				}
+				leftDoorStatus.setText(leftDoorSwitch.getStatusString());
+				rightDoorSwitch = (Switch) in.readObject();
+				if(rightDoorSwitch.getSwitchState()){
+					rightDoorBtn.setSelected(true);
+				}
+				rightDoorStatus.setText(rightDoorSwitch.getStatusString());
+				
+				key = (IgnitionKey) in.readObject();
+				if(key.getState() == IgnitionKey.keyState.OFF){
+					keyOff.setSelected(true);
+					keyStatus.setText("OFF");
+					}
+				else if(key.getState() == IgnitionKey.keyState.ACC){
+					 keyAcc.setSelected(true);
+					 keyStatus.setText("ACC");
+				}
+				else if(key.getState() == IgnitionKey.keyState.START){
+					keyStart.setSelected(true);
+					keyStatus.setText("START");
+				}
+				siren = (AlarmSiren) in.readObject();
+				alarmStatus.setText(siren.getStatusString());
+				carEngine = (Engine) in.readObject();
+				engineFeedBackLabel.setText(carEngine.getStatusString());
+				in.close();
+				//giving feedback to the user
+				hintText.setText("System state loaded");
+			}
+			catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+		else hintText.setText("No save found!");	
+	}
+	public void readCode() throws ClassNotFoundException{
+		Path inPath = Paths.get("code.cod");
+		if(Files.exists(inPath)) {
+			try {
+				FileInputStream fin = new FileInputStream("code.cod");
+				ObjectInputStream in = new ObjectInputStream(fin);
+				code = (String) in.readObject();
+				in.close();
+			}
+			catch(IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	public void writeCode(){
+		try{
+			FileOutputStream fout = new FileOutputStream("code.cod");
+			ObjectOutputStream out = new ObjectOutputStream(fout);
+			out.writeObject(code);
+			out.close();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 }
--- src/prog3_hw_antitheft/Window.java
+++ src/prog3_hw_antitheft/Window.java
@@ -25,11 +25,9 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
-//TODO : OUTSORCING LOGIC TO A SEPARATE JAVA
 
 public class Window extends JFrame implements ActionListener{
 	private static final long serialVersionUID = 4601973850611220034L;
-	public boolean isActive = true;
 	
 	//buttons for inputs
 	JRadioButton leftDoorBtn ;
@@ -65,26 +63,18 @@ public class Window extends JFrame implements ActionListener{
 	JLabel alarmStatus;
 	JLabel keyStatus;
 	JLabel keyStatusTitleLabel;
-	CountdownTimer timer;
-	EngineTimer engTimer;
+
 	
 	JLabel hintText;
 	
-	JTextField codeField;
+	JPasswordField codeField;
 
-	//components of the system
-	Switch hoodSwitch = new Switch();
-	Switch leftDoorSwitch = new Switch();
-	Switch rightDoorSwitch = new Switch();
-	IgnitionKey key = new IgnitionKey();
-	AlarmSiren siren = new AlarmSiren();
-	Engine carEngine = new Engine();
-	
-	static String code;
-	String lastEnteredCode;
+	Logic sysLogic;
+
 	Window(){
+		sysLogic = new Logic();
 		try{
-			readCode();
+			sysLogic.readCode();
 		}
 		catch(ClassNotFoundException ex) {
 			ex.printStackTrace();
@@ -125,16 +115,16 @@ public class Window extends JFrame implements ActionListener{
 		statusPanel = new JPanel();
 		statusTitleLabel = new JLabel("System Status:",SwingConstants.CENTER);
 		enginerunningLabel = new JLabel("Engine",SwingConstants.CENTER);
-		engineFeedBackLabel = new JLabel(carEngine.getStatusString(),SwingConstants.CENTER);
+		engineFeedBackLabel = new JLabel(sysLogic.getEngineStatus(),SwingConstants.CENTER);
 		masterStatus = new JLabel("ARMED",SwingConstants.CENTER);
 		leftDoorTitleLabel = new JLabel("Left door",SwingConstants.CENTER);
-		leftDoorStatus = new JLabel(leftDoorSwitch.getStatusString(),SwingConstants.CENTER);
+		leftDoorStatus = new JLabel(sysLogic.getLDoorStatus(),SwingConstants.CENTER);
 		rightDoorTitleLabel = new JLabel("Right door",SwingConstants.CENTER);
-		rightDoorStatus = new JLabel(rightDoorSwitch.getStatusString(),SwingConstants.CENTER);
+		rightDoorStatus = new JLabel(sysLogic.getRDoorStatus(),SwingConstants.CENTER);
 		hoodTitleLabel = new JLabel("Hood",SwingConstants.CENTER);
-		hoodStatus = new JLabel(hoodSwitch.getStatusString(),SwingConstants.CENTER);
+		hoodStatus = new JLabel(sysLogic.getHoodStatus(),SwingConstants.CENTER);
 		alarmStatusTitleLabel = new JLabel("Alarm Siren",SwingConstants.CENTER);
-		alarmStatus = new JLabel(siren.getStatusString(),SwingConstants.CENTER);
+		alarmStatus = new JLabel(sysLogic.getSirenStatus(),SwingConstants.CENTER);
 		keyStatusTitleLabel = new JLabel("Ignition key",SwingConstants.CENTER);
 		keyStatus = new JLabel("LOCK",SwingConstants.CENTER);
 		
@@ -191,7 +181,7 @@ public class Window extends JFrame implements ActionListener{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try{
-					read();
+					sysLogic.read(masterStatus, hoodStatus, leftDoorStatus, rightDoorStatus, keyStatus, alarmStatus, engineFeedBackLabel, hintText, hoodBtn, leftDoorBtn, rightDoorBtn, keyOff, keyAcc, keyStart);
 				}
 				catch(Exception ex){
 					ex.printStackTrace();
@@ -200,7 +190,7 @@ public class Window extends JFrame implements ActionListener{
 		});
 		writeBtn.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-				write();
+				sysLogic.write(hintText);
 			}
 		});
 		IOPanel.add(writeBtn);
@@ -223,17 +213,9 @@ public class Window extends JFrame implements ActionListener{
 			public void actionPerformed(ActionEvent e) {
 				//if the hood is open
 				if(e.getSource()==hoodBtn){
-					//if the switch is open, we close it, if its not, we open it (Note: switches are closed by default)
-					if(hoodSwitch.getSwitchState()) {
-						hoodSwitch.setSwitchState(false);
-						hoodStatus.setText(hoodSwitch.getStatusString());
-					}
-					else {
-						hoodSwitch.setSwitchState(true);
-						hoodStatus.setText(hoodSwitch.getStatusString());
-					}
+					sysLogic.toggleHood(hoodStatus);
 				}
-				checkAlarm();
+				sysLogic.checkAlarm(alarmStatus,hintText);
 			}
 		};
 		hoodBtn.addActionListener(hoodListener);
@@ -241,30 +223,18 @@ public class Window extends JFrame implements ActionListener{
 		ActionListener lDoorListener = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(e.getSource()==leftDoorBtn) {
-					if(leftDoorSwitch.getSwitchState()) {
-						leftDoorSwitch.setSwitchState(false);
-						leftDoorStatus.setText(leftDoorSwitch.getStatusString());
-					}
-					else
-						leftDoorSwitch.setSwitchState(true);
-						leftDoorStatus.setText(leftDoorSwitch.getStatusString());
+					sysLogic.toggleLeftDoor(leftDoorStatus);
 				}
-				checkAlarm();
+				sysLogic.checkAlarm(alarmStatus,hintText);
 			}
 		};
 		leftDoorBtn.addActionListener(lDoorListener);
 		ActionListener rDoorListener = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(e.getSource()==rightDoorBtn) {
-					if(rightDoorSwitch.getSwitchState()) {
-						rightDoorSwitch.setSwitchState(false);
-						rightDoorStatus.setText(rightDoorSwitch.getStatusString());
-					}
-					else
-						rightDoorSwitch.setSwitchState(true);
-						rightDoorStatus.setText(rightDoorSwitch.getStatusString());
+					sysLogic.toggleRightDoor(rightDoorStatus);
 				}
-				checkAlarm();
+				sysLogic.checkAlarm(alarmStatus,hintText);
 			}
 		};
 		rightDoorBtn.addActionListener(rDoorListener);
@@ -284,14 +254,7 @@ public class Window extends JFrame implements ActionListener{
 		armBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e){
-				if(armable()){
-					isActive = true;
-					masterStatus.setText("ARMED");
-					hintText.setText("System successfully armed");
-				}
-				else{
-					hintText.setText("The system cannot be armed now");
-				}
+				sysLogic.arm(masterStatus, hintText);
 			}
 		});
 		armPanel.add(armBtn);
@@ -306,50 +269,18 @@ public class Window extends JFrame implements ActionListener{
 				
 				ActionListener keyListener = new ActionListener() {
 					public void actionPerformed(ActionEvent e){
-						if(isActive){
-							//if the key is touched with an armed system, we give an instant alarm
-							timer = new CountdownTimer(0,siren,alarmStatus);
-							siren.setSiren(true);
-							alarmStatus.setText(siren.getStatusString());
-							hintText.setText("You turned the key while the system was armed. Enter the code to disarm the system!");
-						}
+						//check whether the key was tampered with
+						sysLogic.keyTamperCheck(alarmStatus,hintText);
 						//in real life, the ignition cylinder has a spring in it, which returns the key to "ACC" from "START" in order to avoid cranking while the engine is running. This behavior is coded here
 						if(e.getSource()==keyOff) {
-							keyStatus.setText("LOCK");
-							key.setState(IgnitionKey.keyState.OFF);
-							if(carEngine.getStatus()){
-								carEngine.stop();
-								engineFeedBackLabel.setText(carEngine.getStatusString());
-								hintText.setText("The engine is now off");
-							}
+							sysLogic.keyOff(keyStatus, engineFeedBackLabel, hintText);
 						}
 						else if(e.getSource()==keyAcc) {
-							keyStatus.setText("ACC");
-							key.setState(IgnitionKey.keyState.ACC);
+							sysLogic.keyAcc(keyStatus);
 						}
 						else if(e.getSource()==keyStart) {
-							keyStatus.setText("START");
-							key.setState(IgnitionKey.keyState.START);
-							//if the alarm is not armed and the engine is not running, we can start it up
-							if(!isActive && !carEngine.getStatus() && carEngine.getStartable()){
-								carEngine.start();
-								engineFeedBackLabel.setText(carEngine.getStatusString());
-								hintText.setText("The engine is now started");
-								key.setState(IgnitionKey.keyState.ACC);
-								keyStatus.setText("ACC");
-								keyAcc.setSelected(true);
-							}
-							else if(carEngine.getStatus()){
-								hintText.setText("Don't crank while the engine is running!");
-								key.setState(IgnitionKey.keyState.ACC);
-								keyStatus.setText("ACC");
-								keyAcc.setSelected(true);
-							}
-							else{
-								key.setState(IgnitionKey.keyState.ACC);
-								keyStatus.setText("ACC");
-								keyAcc.setSelected(true);
-							}
+							sysLogic.keyStart(keyStatus, engineFeedBackLabel, hintText);
+							keyAcc.setSelected(true);
 						}	
 					}
 				};
@@ -382,28 +313,11 @@ public class Window extends JFrame implements ActionListener{
 				codePanel = new JPanel();
 				codeField = new JPasswordField();
 				codeField.setColumns(10);
-				//engTimer = new EngineTimer(15,carEngine);
 				JButton submitButton = new JButton("OK");
 				submitButton.addActionListener(new ActionListener() {
 					@Override
 					public void actionPerformed(ActionEvent e){
-						//if the doors are closed, we can't enter a code because its simply not possible in real life
-						lastEnteredCode = codeField.getText();
-						codeField.setText("");
-						System.out.println(lastEnteredCode);
-						//if the siren is already running or the timer is counting down
-						if((siren.getStatus() || (timer != null && timer.isRunning())) &&  lastEnteredCode.equals(code)){
-							//disarming the system
-							timer.stop();
-							siren.setSiren(false);
-							alarmStatus.setText(siren.getStatusString());
-							isActive = false;
-							masterStatus.setText("DISARMED");
-							hintText.setText("System disarmed. You can go ahead and start the engine in 15 seconds");
-							carEngine.setStartable(true);
-							engTimer = new EngineTimer(15,carEngine,hintText);
-							engTimer.start();
-						}	
+						sysLogic.enterCode(codeField, alarmStatus, masterStatus, hintText);
 					}
 				});
 				
@@ -448,13 +362,13 @@ public class Window extends JFrame implements ActionListener{
 		changeCode.addActionListener(new ActionListener() {
 		    @Override
 		    public void actionPerformed(ActionEvent e) {
-		        final NewCode newc = new NewCode(code);
+		        final NewCode newc = new NewCode(sysLogic.getCode());
 		        // Wait for the NewCode window to be disposed before updating the code
 		        newc.addWindowListener(new WindowAdapter() {
 		            @Override
 		            public void windowClosed(WindowEvent e) {
-		                code = newc.getUpdatedCode();
-		                writeCode();
+		                sysLogic.setCode(newc.getUpdatedCode());
+		                sysLogic.writeCode();
 		            }
 		        });
 		    }
@@ -462,141 +376,8 @@ public class Window extends JFrame implements ActionListener{
 		codeChangePanel.add(changeCode);
 	}
 
-	public boolean armable(){
-		if(!isActive && !carEngine.getStatus() && !hoodSwitch.getSwitchState() && !leftDoorSwitch.getSwitchState() && !rightDoorSwitch.getSwitchState() && key.getState()==IgnitionKey.keyState.OFF){
-			return true;
-		}
-		return false;
-	}
+
 	public void actionPerformed(ActionEvent e) {
 		//empty
-	}
-	//logic for triggering the alarm
-	public void checkAlarm() {
-		//when the system is active, we need to check for triggering conditions
-		if(isActive){
-			if(hoodSwitch.getSwitchState()) {
-				//workaround, we have an instant alarm in case the hood is opened
-				timer = new CountdownTimer(0,siren,alarmStatus);
-				siren.setSiren(true);
-				alarmStatus.setText(siren.getStatusString());
-				hintText.setText("You opened the hood while the system was armed. Enter the code to disarm the system!");
-			}
-			//if we opened any of the doors while the system is active
-			else if(leftDoorSwitch.getSwitchState() || rightDoorSwitch.getSwitchState()) {
-				hintText.setText("You opened a door. Enter the code in 30 seconds to disarm the system!");
-				//countdown
-				timer = new CountdownTimer(30,siren,alarmStatus);
-				timer.start();
-			}
-		}
-	}
-	public void write(){
-		try{
-			//initializing the output data stream
-			FileOutputStream fileOut = new FileOutputStream("alarm.ser");
-			ObjectOutputStream out = new ObjectOutputStream(fileOut);
-			//writing the objects to the file
-			out.writeObject(isActive);
-			out.writeObject(lastEnteredCode);
-			out.writeObject(hoodSwitch);
-			out.writeObject(leftDoorSwitch);
-			out.writeObject(rightDoorSwitch);
-			out.writeObject(key);
-			out.writeObject(siren);
-			out.writeObject(carEngine);
-			out.close();
-			hintText.setText("System state saved");
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-	}
-	public void read()throws ClassNotFoundException{
-		//checking whether the file to read from exists
-		Path inpPath = Paths.get("alarm.ser");
-		if(Files.exists(inpPath)){
-			//if the file exists
-			try{
-				//initializing the data stream
-				FileInputStream fileIn = new FileInputStream("alarm.ser");
-				ObjectInputStream in = new ObjectInputStream(fileIn);
-				//reading objects and setting feedback
-				isActive = (Boolean) in.readObject();
-				if(isActive){
-					masterStatus.setText("ARMED");
-				}
-				else{
-					 masterStatus.setText("DISARMED");
-				}
-				lastEnteredCode = (String) in.readObject();
-				hoodSwitch = (Switch) in.readObject();
-				hoodStatus.setText(hoodSwitch.getStatusString());
-				if(hoodSwitch.getSwitchState()){
-					hoodBtn.setSelected(true);
-				}
-				leftDoorSwitch = (Switch) in.readObject();
-				if(leftDoorSwitch.getSwitchState()){
-					leftDoorBtn.setSelected(true);
-				}
-				leftDoorStatus.setText(leftDoorSwitch.getStatusString());
-				rightDoorSwitch = (Switch) in.readObject();
-				if(rightDoorSwitch.getSwitchState()){
-					rightDoorBtn.setSelected(true);
-				}
-				rightDoorStatus.setText(rightDoorSwitch.getStatusString());
-				
-				key = (IgnitionKey) in.readObject();
-				if(key.getState() == IgnitionKey.keyState.OFF){
-					keyOff.setSelected(true);
-					keyStatus.setText("OFF");
-					}
-				else if(key.getState() == IgnitionKey.keyState.ACC){
-					 keyAcc.setSelected(true);
-					 keyStatus.setText("ACC");
-				}
-				else if(key.getState() == IgnitionKey.keyState.START){
-					keyStart.setSelected(true);
-					keyStatus.setText("START");
-				}
-				siren = (AlarmSiren) in.readObject();
-				alarmStatus.setText(siren.getStatusString());
-				carEngine = (Engine) in.readObject();
-				engineFeedBackLabel.setText(carEngine.getStatusString());
-				in.close();
-				//giving feedback to the user
-				hintText.setText("System state loaded");
-			}
-			catch(IOException e){
-				e.printStackTrace();
-			}
-		}
-		else hintText.setText("No save found!");	
-	}
-	public void writeCode(){
-		try{
-			FileOutputStream fout = new FileOutputStream("code.cod");
-			ObjectOutputStream out = new ObjectOutputStream(fout);
-			out.writeObject(code);
-			out.close();
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-	}
-
-	public void readCode() throws ClassNotFoundException{
-		Path inPath = Paths.get("code.cod");
-		if(Files.exists(inPath)) {
-			try {
-				FileInputStream fin = new FileInputStream("code.cod");
-				ObjectInputStream in = new ObjectInputStream(fin);
-				code = (String) in.readObject();
-				in.close();
-			}
-			catch(IOException ex) {
-				ex.printStackTrace();
-			}
-		}
 	}
 }
